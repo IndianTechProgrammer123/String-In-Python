3f5a10a5dde1ea5ab96c8d473a3a4161156dd7f3
Indian Tech Programmer <112259649+IndianTechProgrammer123@users.noreply.github.com>
2022-09-06 20:04:43 +0530

    Add files via upload
    video link - https://youtube.com/shorts/ToZzHfcIJiQ

---
+++ string.py
@@ -0,0 +1,12 @@
+# string in python 
+
+# 1. What is string?
+# String is a collection of character.
+
+# 2. How to make string?
+# String can be created by single quote or double quotes.
+
+# 3. How to use string?
+# Use of string Ex: Hello World
+print('Hello World printed by using single quote')
+print("Hello World printed by using double quotes")
